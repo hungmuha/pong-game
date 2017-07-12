9ac2cff753d5c072f26181a7983910a0ecdda010
hungmuha <hung.soboss@gmail.com>
2017-07-12 15:35:33 -0600

    working on restart the ball when it crossed the line

--- js/app.js
+++ js/app.js
@@ -25,6 +25,9 @@ var dy=2;
 //for canvas 
 var canvas;
 var ctx;
+//players score
+var p1Score=0;
+var p2Score=0;
 
 
 
@@ -36,12 +39,19 @@ function init(){ //game board created
 	return intervalId
 }
 
-function initPaddle(){ //paddle created
+function initPaddle(){ //paddle 1 created
 	paddley=boxHeight/2;
-	paddleh=50;
+	paddleh=80;
 	paddlew=10;
 
 }
+
+function initPaddle2(){ //paddle 2 created
+	paddley2=boxHeight/2;
+	paddleh2=80;
+	paddlew2=10;
+}
+
 window.addEventListener("keypress",key)
 function key(event){
 	var k = event.keyCode;
@@ -51,22 +61,38 @@ function key(event){
 	if (k==111){
 		paddley-=40;
 	}
+
+	if (k==97){
+		paddley2+=40;
+	}
+	if (k==113){
+		paddley2-=40;
+	}
 }
 
 function draw(){
 	clear();
 	arc(x,y,10);
 	rect(boxWidth-paddlew,paddley,paddlew,paddleh);
+	rect(0,paddley2,paddlew2,paddleh2);
 
 	if(x+dx<0){
+		if(y>paddley2 && y<paddley2+paddleh2){
 		dx=-dx;
+		}else{
+
+			console.log("player 1 win")
+		}
 	}else if(x+dx>boxWidth){
 		if(y>paddley && y<paddley+paddleh){
 			dx=-dx;
 		}else{
-			clearInterval(intervalId);
+			console.log("player 2 win")
+			// clearInterval(intervalId);
 		}
 	}
+
+
 	if(y+dy>boxHeight||y+dy<0){
 		dy=-dy;
 	}
@@ -94,6 +120,8 @@ function rect(a,b,c,d){
 
 init();
 initPaddle();
+initPaddle2();
+
 
 
 
